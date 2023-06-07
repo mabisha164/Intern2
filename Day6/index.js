26e406e9da9303e493a8c8a859d8b561a4d3c93b
mabisha164 <mabisha164@gmail.com>
2023-06-07 09:22:58 +0545

    Array methods

---
+++ Day6/index.js
@@ -0,0 +1,51 @@
+// // Map Array
+// let number = [22,23,14,25,26,27]
+// let arr = number.map((value,index,array)=>{
+//     console.log(value,index)
+//     return value+index
+// })
+// console.log (arr)
+
+// // Filter Array
+ 
+// let value =[1,3,7,23,14,25,22,10,8,12]
+// let num = value.filter((i)=>{
+//     return i > 20
+// })
+// console.log(num)
+
+// // For each array
+// let nums = [2,4,6,8,10,12,14,22,13,11]
+
+//  nums.forEach((n) =>{
+//     console.log (n)
+//  });
+
+// //  Map Array
+
+// const numbers = [2,4,6,8,3,5,7,9]
+  
+// const squareNum = numbers.map((num)=>num*num);
+//      console.log(squareNum);
+  
+
+//  const stringNumbers = numbers.map((num)=>num.toString());
+//  console.log (stringNumbers);
+
+// //  Array Reduce Method
+// let count =[2,12,10,3,5,12,7]
+
+// let newCount=count.reduce((h1,h2)=>{
+//     return h1+h2
+// })
+// console.log(newCount)
+
+
+
+// Push and pop
+const fruits=['apple','banana','orange','litchi'];
+// fruits.push('watremelon');
+
+
+fruits.pop();
+console.log(fruits);
